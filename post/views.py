17ce2b9ec124717b4fd15f993409bfeee274aafc
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 15:28:04 +0300

    FEAT: Create PostViewSet

--- post/views.py
+++ post/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(ModelViewSet):
+    """View set for the Post model."""
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
