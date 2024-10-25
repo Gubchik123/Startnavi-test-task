690da49f754f69b9492681dc0f18b1c0c1ef2e16
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 15:30:46 +0300

    CHORE: Start Django app "comment"

---
+++ comment/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class CommentConfig(AppConfig):
+    """App config for the 'comment' app."""
+
+    name = "comment"
+    default_auto_field = "django.db.models.BigAutoField"
--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     # Local apps
     "post",
+    "comment",
 ]
 
 MIDDLEWARE = [
