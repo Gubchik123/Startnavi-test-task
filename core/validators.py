2e275176caaf4e929e127c4c27cc868ffeb89ef1
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 17:30:08 +0300

    FEAT: Create validate_content_(text) function
    in new core/validators.py

---
+++ core/validators.py
@@ -0,0 +1,14 @@
+import re
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+
+def validate_content_(text):
+    """Validate that the given text does not contain censored words."""
+    text = text.lower()
+    for censored_word in settings.CENSORED_WORDS:
+        if censored_word in text:
+            raise ValidationError(
+                f"Content contains censored word '{censored_word}' !"
+            )
