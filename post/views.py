134418bc912680de66068404b40776ca34863c74
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 18:12:46 +0300

    FEAT: Add comments_analytics action to the PostViewSet

--- post/views.py
+++ post/views.py
@@ -1,9 +1,13 @@
+from django.http import JsonResponse
+from django.utils.dateparse import parse_date
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from core.permissions import IsAuthorOrReadOnly
 
 from .models import Post
 from .serializers import PostSerializer
+from .services import get_comments_analytics
 
 
 class PostViewSet(ModelViewSet):
@@ -12,3 +16,16 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthorOrReadOnly]
+
+    @action(detail=True, methods=["get"])
+    def comments_analytics(self, request, pk=None):
+        """Return analytics about comments
+        that were added to posts during a certain period."""
+        date_from = parse_date(request.query_params.get("date_from", ""))
+        date_to = parse_date(request.query_params.get("date_to", ""))
+
+        if not date_from or not date_to:
+            return JsonResponse({"error": "Invalid date range"}, status=400)
+        return JsonResponse(
+            get_comments_analytics(pk, date_from, date_to), safe=False
+        )
