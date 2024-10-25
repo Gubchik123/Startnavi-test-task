435ad6e01ada11a8fe6100a859dec9c186687a74
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 15:44:49 +0300

    FEAT: Create CommentAdmin

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Admin class for the Comment model."""
+
+    search_fields = ["content"]
+    search_help_text = "Search by content"
+    list_display = [
+        "mini_content",
+        "post",
+        "author",
+        "parent",
+        "is_blocked",
+        "created_at",
+        "updated_at",
+    ]
+    list_display_links = ["mini_content"]
+    list_filter = ["is_blocked", "created_at", "updated_at"]
+
+    def mini_content(self, comment):
+        """Return the first 50 characters of the given comment content."""
+        return comment.content[:50]
