dded976a27206ac361e98d012e9bce3f6e17fe76
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 15:13:13 +0300

    FEAT: Create PostAdmin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Admin class for the Post model."""
+
+    search_fields = ("title", "content")
+    search_help_text = "Search by title or content"
+    list_display = (
+        "title",
+        "author",
+        "auto_comment_answer",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = ("title",)
+    list_filter = ("auto_comment_answer", "created_at", "updated_at")
