6e63e3aed56f983a01bebe00db3dc7b1a695bae7
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 16:45:47 +0300

    FEAT: Create CommentViewSet

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from core.permissions import IsAuthorOrReadOnly
+
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+class CommentViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    """Write-only viewset for the Comment model."""
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly]
