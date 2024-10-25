cf11532e4f62c1556886f88eda528d1a4ea9ba79
Nikita <nikita.gubarev31@gmail.com>
2024-10-25 15:41:09 +0300

    FEAT: Create Comment model
    Make initial migration

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from post.models import Post
+
+
+class Comment(models.Model):
+    """Model representing a comment on a post."""
+
+    content = models.TextField()
+    is_blocked = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    parent = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="replies",
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
+
+    def __str__(self):
+        """Returns the author and post as the string representation of the Comment model"""
+        return f"{self.user} - {self.post}"
+
+    class Meta:
+        """Meta options for the Comment model."""
+
+        ordering = ["-created_at", "-updated_at"]
