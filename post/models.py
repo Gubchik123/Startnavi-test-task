6c633a74c9c847ee5566bb4a8b5b5b4eed842dae
Nikita <nikita.gubarev31@gmail.com>
2024-10-26 12:39:42 +0300

    FEAT: Update the Post model's comment_answer_delay_mins field to use PositiveIntegerField
    Make migration

--- post/models.py
+++ post/models.py
@@ -11,7 +11,7 @@ class Post(TimeStampedModel):
     title = models.CharField(max_length=255)
     content = models.TextField(validators=[validate_content_])
     auto_comment_answer = models.BooleanField(default=False)
-    comment_answer_delay_mins = models.IntegerField(default=0)
+    comment_answer_delay_mins = models.PositiveIntegerField(default=0)
 
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
