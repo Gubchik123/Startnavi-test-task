43409f14bfadfc1330f7c93e69aac6522ac6a0ac
Nikita <nikita.gubarev31@gmail.com>
2024-10-26 13:43:59 +0300

    TEST: Create CommentViewSetAPITestCase
    in new comment/tests/test_views.py

---
+++ comment/tests/test_views.py
@@ -0,0 +1,60 @@
+from rest_framework.test import APITestCase
+
+from post.models import Post
+from comment.models import Comment
+from core.test_mixins import IsAuthorOrReadOnlyPermissionTestMixin
+
+
+class CommentViewSetAPITestCase(
+    IsAuthorOrReadOnlyPermissionTestMixin, APITestCase
+):
+    """Test cases for the CommentViewSet."""
+
+    url = "/api/v1/comments/"
+
+    @classmethod
+    def setUpTestData(cls):
+        """Creates test data."""
+        super().setUpTestData()
+        post = Post.objects.create(
+            title="Post title", content="Post content", author=cls.author
+        )
+        cls.obj = Comment.objects.create(
+            content="Comment content", post=post, author=cls.author
+        )
+
+    def test_list_comments(self):
+        """Test that the list endpoint is disabled."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 405)
+
+    def test_get_comment(self):
+        """Test that the detail endpoint is disabled."""
+        response = self.client.get(f"{self.url}{self.obj.id}/")
+        self.assertEqual(response.status_code, 405)
+
+    def test_create_with_censored_word(self):
+        """Test the creation of the comment with a censored word."""
+        self.client.force_authenticate(user=self.user)
+        data = self._get_valid_data()
+        data["content"] = "Something bad"
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(
+            Comment.objects.get(content="Something bad").is_blocked
+        )
+
+    def _get_valid_data(self):
+        """Returns valid data for the comment creation."""
+        post = Post.objects.create(
+            title="Post title", content="Post content", author=self.user
+        )
+        return {
+            "content": "Comment content",
+            "post": post.id,
+            "author": self.user.id,
+        }
+
+    def _get_invalid_data(self):
+        """Returns invalid data for the comment creation."""
+        return {"content": ""}
