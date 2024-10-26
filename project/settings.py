b7e8bc839d538adcc2d0bda9ead8b3b8509ea1ec
Nikita <nikita.gubarev31@gmail.com>
2024-10-26 13:43:18 +0300

    SETTINGS: Remove default pagination from the REST_FRAMEWORK

--- project/settings.py
+++ project/settings.py
@@ -124,8 +124,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 25,
 }
 
 SPECTACULAR_SETTINGS = {
